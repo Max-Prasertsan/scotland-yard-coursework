d49922ee4217b88b904743d2d58696d390a73c0d
Max-Prasertsan <zb20249@bristol.ac.uk>
2021-03-15 15:52:42 +0700

    Finished with attributes and start working on constructor.

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -1,11 +1,14 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
+import java.util.*;
+
+import uk.ac.bris.cs.scotlandyard.model.Piece.*;
 
 import javax.annotation.Nonnull;
-
-import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
 /**
  * cw-model
@@ -13,13 +16,67 @@ import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
  */
 public final class MyGameStateFactory implements Factory<GameState> {
 
-	@Nonnull @Override public GameState build(
-			GameSetup setup,
-			Player mrX,
-			ImmutableList<Player> detectives) {
-		// TODO
-		throw new RuntimeException("Implement me!");
-
+	@Nonnull @Override public GameState build(GameSetup setup, Player mrX, ImmutableList<Player> detectives) {
+		return new MyGameState(setup, ImmutableSet.of(MrX.MRX), ImmutableList.of(), mrX, detectives);
 	}
 
+	private final class MyGameState implements GameState {
+		private GameSetup setup;
+		private ImmutableSet<Piece> remaining;
+		private ImmutableList<LogEntry> log;
+		private Player mrX;
+		private List<Player> detectives;
+		private ImmutableList<Player> everyone;
+		private ImmutableSet<Move> moves;
+		private ImmutableSet<Piece> winner;
+
+		private MyGameState(
+				final GameSetup setup,
+				final ImmutableSet<Piece> remaining,
+				final ImmutableList<LogEntry> log,
+				final Player mrX,
+				final List<Player> detectives){
+			this.setup = setup;
+			this.remaining = remaining;
+			this.log = log;
+			this.mrX = mrX;
+			this.detectives = detectives;
+
+			// For checking parameters.
+			if (setup.rounds.isEmpty()) throw new IllegalArgumentException("Round is empty.");
+		}
+
+
+		@Override public GameSetup getSetup() {
+			return setup;
+		}
+
+		@Override public ImmutableSet<Piece> getPlayers() {
+			return remaining;
+		}
+
+		@Override public Optional<Integer> getDetectiveLocation(Detective detective) {
+			return null;
+		}
+
+		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			return Optional.empty();
+		}
+
+		@Override public ImmutableList<LogEntry> getMrXTravelLog() {
+			return log;
+		}
+
+		@Override public ImmutableSet<Piece> getWinner() {
+			return winner;
+		}
+
+		@Override public ImmutableSet<Move> getAvailableMoves() {
+			return moves;
+		}
+
+		@Override public GameState advance(Move move) {
+			return null;
+		}
+	}
 }
