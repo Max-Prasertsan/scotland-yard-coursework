a65f6ca381ff8c77cc74ca30824bb09d519699a7
manami <manami2622@gmail.com>
2021-03-21 14:42:36 +0900

    copied

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -2,30 +2,22 @@ package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-import java.util.*;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
-import uk.ac.bris.cs.scotlandyard.model.Move.*;
-import uk.ac.bris.cs.scotlandyard.model.Piece.*;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
+import java.util.*;
+
+import uk.ac.bris.cs.scotlandyard.model.Piece.*;
 
 import javax.annotation.Nonnull;
-
 
 /**
  * cw-model
  * Stage 1: Complete this class
  */
-
-
-
-
 public final class MyGameStateFactory implements Factory<GameState> {
 
-	@Nonnull
-	@Override public GameState build(GameSetup setup, Player mrX, ImmutableList<Player> detectives) {
-		// TODO
-			return new MyGameState (setup, ImmutableSet.of(Piece.MrX.MRX), ImmutableList.of(), mrX, detectives);
-
+	@Nonnull @Override public GameState build(GameSetup setup, Player mrX, ImmutableList<Player> detectives) {
+		return new MyGameState(setup, ImmutableSet.of(MrX.MRX), ImmutableList.of(), mrX, detectives);
 	}
 
 	private final class MyGameState implements GameState {
@@ -43,62 +35,74 @@ public final class MyGameStateFactory implements Factory<GameState> {
 				final ImmutableSet<Piece> remaining,
 				final ImmutableList<LogEntry> log,
 				final Player mrX,
-				final List<Player> detectives) {
+				final List<Player> detectives){
 			this.setup = setup;
 			this.remaining = remaining;
 			this.log = log;
 			this.mrX = mrX;
 			this.detectives = detectives;
 
-			// For checking parameters.
+			// checking part
+			// Check if the round is empty.
 			if (setup.rounds.isEmpty()) throw new IllegalArgumentException("Round is empty.");
+
+			// Check if Mr X value is empty/null.
+			if (mrX == null) throw new NullPointerException("Mr X is empty.");
+
+			// Check that Detectives are not empty.
+			if (detectives == null) throw new NullPointerException("Detectives are empty.");
+
+			// Check for 2 Mr X
+			for (Player detective : detectives) {
+				if (detective.isMrX()) throw new IllegalArgumentException("There's more than 1 Mr X.");
+			}
+
+			// Check for swapped Mr X
+			if (mrX.isDetective()) throw new IllegalArgumentException("Mr X is invalid");
+
+			// Check for Duplicate detectives
+			for (int i = 0; i < detectives.size(); i++){
+				for (int j = 0; j < detectives.size(); j++){
+					if(i != j){
+						if (detectives.get(i).equals(detectives.get(j))) throw new IllegalArgumentException("Duplicate detectives");
+					}
+				}
+			}
+
 		}
 
 
-		@Override
-		public GameSetup getSetup() {
+		@Override public GameSetup getSetup() {
 			return setup;
 		}
 
-		@Override
-		public ImmutableSet<Piece> getPlayers() {
+		@Override public ImmutableSet<Piece> getPlayers() {
 			return remaining;
 		}
 
-		@Override
-		public Optional<List<Player>> getDetectiveLocation(Piece.Detective detective) {
-			// For all detectives, if Detective#piece == detective, then return the location in an Optional.of();
-			// otherwise, return Optional.empty();
-			if(detective.isDetective()){
-				return Optional.of(ImmutableBoard.detectiveLocations.get(detective));
-			}else{
-				return Optional.empty();
-			}
-		}
-
-		@Override
-		public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+		@Override public Optional<Integer> getDetectiveLocation(Detective detective) {
 			return Optional.empty();
 		}
 
-		@Override
-		public ImmutableList<LogEntry> getMrXTravelLog() {
+		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			return Optional.empty();
+		}
+
+		@Override public ImmutableList<LogEntry> getMrXTravelLog() {
 			return log;
 		}
 
-		@Override
-		public ImmutableSet<Piece> getWinner() {
+		@Override public ImmutableSet<Piece> getWinner() {
 			return winner;
 		}
 
-		@Override
-		public ImmutableSet<Move> getAvailableMoves() {
+		@Override public ImmutableSet<Move> getAvailableMoves() {
 			return moves;
 		}
 
-		@Override
-		public GameState advance(Move move) {
+		@Override public GameState advance(Move move) {
 			return null;
 		}
 	}
 }
+
