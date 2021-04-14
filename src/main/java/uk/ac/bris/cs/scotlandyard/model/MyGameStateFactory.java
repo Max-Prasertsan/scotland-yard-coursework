3ba2ffb9e67f0fa90c78990ad8f4783f2faf5284
Max-Prasertsan <zb20249@bristol.ac.uk>
2021-04-14 17:53:46 +0700

    update on advance

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -354,38 +354,40 @@ public final class MyGameStateFactory implements Factory<GameState> {
 
 		@Override public GameState advance(Move move) {
 			if (!moves.contains(move)) throw new IllegalArgumentException("Illegal move: " + move);
-			// return gamestate after committing that move
-			// check potential problem -> throw illegal
-			// need to have a piece to check if move is being done
-			// remaining pieces
-			// entry list -> for MrX
-			// list of all player
-			// -add travel log to entry list
-			// -check if there is any move -> throw error
-			// -make a visit function -> below
-			// 		-check for destination -> in that visit, need to add for single move/double move
-			// check the original destination
-			// list of all destination -> check size, check if player MrX
-			// need to extract last element and put in second destination -> loop the destinations
-			// make copy of MrX
-			// 		for double move -> for 2 destinations
-			//		check if get to last ticket
-			//		counter to keep ticket
-			//		get the last ticket and break the loop
-			// check for the round
-			// reveal the ticket for MrX
-			// 		-> if have secret no reveal
-			// reveal last ticket for MrX
-			// need to add remaining player back to the 'everyone' list
-			// if player is not MrX -> go through the player list to check destination
-			//		copy of MrX give it ticket detectives
-			// 		loop other player and check if still in game -> add to remaining
-			//		add the remaining detective to the list then MrX
-			// - make a copy of remaining players and log entry list and return myGameState
+			ImmutableList<LogEntry> new_log = null;
+			// condition for Mr X
+			if (move.commencedBy().isMrX()){
+				for (Ticket t : move.tickets()){
+					if (t.equals(Ticket.DOUBLE)){
+						// the move is a double move
+						// need to handle 2 destinations
+						// get only the final location, but subtract 2 tickets used.
+					}
+					else{
+						// total used ticket is 1. Just update current position normally.
+						// make new ticket list to insert into new MrX.
+						// find the final destination of the move, add to current move.visit()
+						Player newMrX = new Player(mrX.piece(), mrX.tickets(), move.visit());
+					}
+				}
+			}
+			// condition for Detectives
+			else{
+				for (Ticket t : move.tickets()){
+					if (t.equals(Ticket.DOUBLE)) throw new IllegalArgumentException("Detective has double move");
+					else{
+						// subtract the used ticket out of detective tickets list
+						// update current location of the detective
+						// create new instant of that detective
+						// add to the remaining list.
+					}
+				}
 
-			// keep it clean
-
-			return null;
+			}
+			// use the new updated version of either MrX or detectives
+			// change in remaining
+			// change in log, add the move that been made by MrX
+			return new MyGameState(setup, remaining, log, mrX, detectives);
 		}
 	}
 }
